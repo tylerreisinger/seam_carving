bc60020a144f873299d39842ba934e021a7a5a60
Tyler Reisinger <reisinger.tyler@gmail.com>
2017-10-28 16:28:16 -0400

    Computation of a central difference based gradient of an image.

---
+++ src/gradient.rs
@@ -0,0 +1,44 @@
+use image::{self, Pixel};
+use cgmath::{Vector2, InnerSpace};
+use std::f64;
+
+#[derive(Debug, Clone)]
+pub struct ImageGradient {
+    pub values: Vec<Vector2<f64>>,
+    pub width: u32,
+    pub height: u32,
+}
+
+impl ImageGradient {
+    pub fn from_luma_image(image: &image::RgbaImage) -> ImageGradient {
+        let (width, height) = (image.width(), image.height());
+
+        let total_size = (width as usize) * (height as usize);
+        let mut gradient = Vec::with_capacity(total_size);
+        gradient.resize(total_size, Vector2::new(0.0, 0.0));
+
+        for y in 0..height {
+            for x in 1..(width - 1) {
+                let index = x + y * width;
+                let val = (image[(x + 1, y)].to_luma().data[0] as f64) -
+                    (image[(x - 1, y)].to_luma().to_luma().data[0] as f64);
+                gradient[index as usize].x = val;
+            }
+        }
+
+        for x in 0..width {
+            for y in 1..(height - 1) {
+                let index = x + y * width;
+                let val = (image[(x, (y + 1))].to_luma().data[0] as f64) -
+                    (image[(x, (y - 1))].to_luma().to_luma().data[0] as f64);
+                gradient[index as usize].y = val;
+            }
+        }
+
+        ImageGradient {
+            values: gradient,
+            width,
+            height,
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 use std::path;
+use cgmath::{Vector2, InnerSpace};
 
 extern crate image;
+extern crate cgmath;
+
+pub mod gradient;
 
 fn load_image(path: &path::Path) -> image::ImageResult<image::RgbaImage> {
     image::open(path).map(|img| img.to_rgba())
@@ -20,4 +24,15 @@ fn main() {
 
 fn process_image(image: image::RgbaImage) {
     println!("{}x{}", image.width(), image.height());
+
+    let gradient = gradient::ImageGradient::from_luma_image(&image);
+
+    let img_buf = gradient
+        .values
+        .iter()
+        .map(|x| ((x / 255.0).magnitude() / f64::sqrt(2.0) * 255.0) as u8)
+        .collect::<Vec<_>>();
+
+    let img = image::GrayImage::from_raw(gradient.width, gradient.height, img_buf).unwrap();
+    img.save("TestOut.png").unwrap();
 }
